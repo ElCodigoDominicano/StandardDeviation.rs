89c0d21d39dfb93e3ab2b2c75fb2a0aa8b512dc1
El Codigo Dominicano <Anburivas@gmail.com>
2024-06-15 23:32:37 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,74 +35,83 @@ impl Number {
     }
 }
 
+#[derive(Debug, PartialEq, Clone)]
+struct UserData {
+    data: Vec<Number>,
+}
+
+impl UserData {
+    fn len(&self) -> f32 { 
+        self.data.len() as f32
+    }
+
+    fn sum_squared(&self) -> f32 {
+        let mut num = 0.0;
+        for x in  &self.sqrt() {
+            num += x;
+        }
+        num
+    }
+
+    fn sum(&self) -> f32 {
+        let mut summed = 0.0;
+        for x in &self.data {
+            let num = x.clone().get_number();
+            summed += num;
+        }
+        summed
+    }
+
+    fn mean(&self) -> f32 {
+        self.sum() / self.len()
+    }
+
+    fn diff(&self) -> Vec<f32> {
+        let mut difference = Vec::new();
+        for x in &self.data {
+            let mut num = x.clone().get_number();
+            num -= self.mean(); 
+            difference.push(num);
+        }
+        difference
+    }
+
+    fn sqrt(&self) -> Vec<f32> {
+        let mut squared = Vec::new();
+        for x in &self.diff() {
+            squared.push(x.powf(2.0));
+        }
+        squared
+    }
+}
+
 fn main() {
     // Standard Deviation, σ: 874.32854145338
     // Standard Deviation, s: 921.62320476188
-    let test: Vec<Number> = vec![
-        Number::Int(1), 
-        Number::Float(5.3), 
-        Number::Int(200), 
-        Number::Float(-533.6), 
-        Number::Int(1000), 
-        Number::Float(2000.9),
-        Number::Int(-1500), 
-        Number::Float(-2.5), 
-        Number::Int(-50),
-        Number::Float(-350.7)
-    ];
-    standard_deviation(test);
-}
-
-fn standard_deviation(data: Vec<Number>) {
-    let sum_of_data = sum(data.clone());
-    let len_of_data = data.len();
-    let mean_of_data = mean(sum_of_data, len_of_data);
-    let difference = diff(data, mean_of_data);
-    let squared = sqrt(difference);
-    let squared_sum = sum_squared(squared);
+     
+    let user_data: UserData = UserData { 
+        data: vec![
+            Number::Int(1), 
+            Number::Float(5.3), 
+            Number::Int(200), 
+            Number::Float(-533.6), 
+            Number::Int(1000), 
+            Number::Float(2000.9),
+            Number::Int(-1500), 
+            Number::Float(-2.5), 
+            Number::Int(-50),
+            Number::Float(-350.7)
+        ]
+    };
     
-    let population_standard_deviation = squared_sum / len_of_data as f32;
-    let sample_standard_deviation = squared_sum / (len_of_data as f32 - 1.0);
-    
-    println!("This is the population standard deviation {:?}", population_standard_deviation.powf(0.5));
-    println!("This is the sample standard deviation {:?}", sample_standard_deviation.powf(0.5));
-}
+    let len = user_data.len(); 
+    let sum_sqrt = user_data.sum_squared();
+    let population = sum_sqrt / len;
+    let sample = sum_sqrt / (len - 1.0);
 
-fn sum_squared(arr: Vec<f32>) -> f32 {
-    let mut num = 0.0;
-    for x in arr {
-        num += x;
-    }
-    num
-}
-
-fn sum(arr: Vec<Number>) -> f32 {
-    let mut summed = 0.0;
-    for x in arr {
-        let num = x.get_number();
-        summed += num;
-    }
-    summed
-}
-
-fn mean(sum_of_data: f32, len_of_data: usize) -> f32 {
-    sum_of_data / len_of_data as f32
-}
-
-fn diff(arr: Vec<Number>, mean_data: f32) -> Vec<f32> {
-    let mut difference = Vec::new();
-    for x in arr {
-        let mut num = x.get_number();
-        num -= mean_data; 
-        difference.push(num);
-    }
-    difference
-}
-
-fn sqrt(arr: Vec<f32>) -> Vec<f32> {
-    let mut squared = Vec::new();
-    for x in arr {
-       squared.push(x.powf(2.0));
-    }
-    squared
+    println!(
+        "Population Deviation {:?}\nSample Deviation {:?}",
+        population.powf(0.5),
+        sample.powf(0.5),
+    );
 }
